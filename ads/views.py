9eae69f8ed5d310e0b61ebbf832d6fb3226e1014
Artem <artem.mikhaylov5593@yandex.ru>
2022-07-10 20:12:39 +0300

    added filters in ads view

--- ads/views.py
+++ ads/views.py
@@ -1,6 +1,5 @@
 import json
 
-import pandas
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -97,8 +96,24 @@ class AdListView(ListView):
     def get(self, request, *args, **kwargs):
         super().get(request, *args, **kwargs)
 
+        categories = request.GET.getlist("cat", [])
+        if categories:
+            self.object_list = self.object_list.filter(category_id__in=categories)
+
+        if request.GET.get("text", None):
+            self.object_list = self.object_list.filter(name__icontains=request.GET.get("text"))
+
+        if request.GET.get("location", None):
+            self.object_list = self.object_list.filter(author__locations__name__icontains=request.GET.get("location"))
+
+        if request.GET.get("price_from", None):
+            self.object_list = self.object_list.filter(price__gte=request.GET.get("price_from"))
+
+        if request.GET.get("price_to", None):
+            self.object_list = self.object_list.filter(price__lte=request.GET.get("price_to"))
+
         self.object_list = self.object_list.select_related("author").order_by("-price")
-        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        paginator = Paginator(self.object_list, settings.REST_FRAMEWORK["PAGE_SIZE"])
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
