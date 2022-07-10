67ef4a4419b7555bfaf0b584bad4c66c83a2d947
Artem <artem.mikhaylov5593@yandex.ru>
2022-07-10 18:53:20 +0300

    added DRF in settings.py and urls.py

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('ads.urls')),
     path('user/', include('users.urls')),
     path('add/', include('db_init.urls')),
