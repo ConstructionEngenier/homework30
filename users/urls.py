cf49d0a721529346111a4a2bceeea5f02e7c32f0
Artem <artem.mikhaylov5593@yandex.ru>
2022-08-14 18:54:59 +0300

    Created JWT urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from users import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', views.UserListView.as_view()),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('create/', views.UserCreateView.as_view()),
     path('<int:pk>/update/', views.UserUpdateView.as_view()),
     path('<int:pk>/delete/', views.UserDeleteView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
